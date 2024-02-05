fb8dba34b068e53a5919f3d63c9de172f6774c26
Oleksandr <romanyyk2004@gmail.com>
2024-02-05 14:05:55 +0200

    add homework-11

---
+++ homework-11/hw11.js
@@ -0,0 +1,8 @@
+const removeElement = (array, value) => {
+    let index = array.indexOf(value);
+    if (index !== -1) return array.splice(index, 1)
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);
